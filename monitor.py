2da8ff31dd55fd65409230c1d2c917599f9e452a
V-Kopio <ville.kopio@gmail.com>
2018-05-28 14:52:40 +0300

    Reformatting a long comment

--- monitor.py
+++ monitor.py
@@ -2,7 +2,8 @@ from sample.ping import ping as responds
 from sample.slack import *
 from data.data import *
 
-# Loops through all hosts from data.py and sends a Slack notification every time a host does not respond
+# Loops through all hosts from data.py and sends a Slack notification
+# every time a host does not respond
 for host in hosts:
     if not responds(host[1]):
         send_slack_notification(slack_webhook_url, slack_message(host))
